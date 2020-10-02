8f31d615d572fadca2b0b554f0633bd0f03a3c4d
Robert Oikle <robert.oikle@canada.ca>
2020-10-02 11:29:20 -0400

    Added build config files for building different viewers

---
+++ config/proximity-viewer-prd.rollup.js
@@ -0,0 +1,16 @@
+import babel from 'rollup-plugin-babel';
+
+export default {
+    input: '../proximity-viewer/main.js',
+    output: {
+        file: '../../web-mapping-prd/proximity-viewer/main.min.js',
+        format: 'iife',
+        name: 'bundle'
+    },
+    plugins: [
+        babel({
+            exclude: 'node_modules/**',
+			configFile: './config/babel.config.js'
+        })
+    ]
+}

b3eb3294709cd5c86ef98f828b55d5ae8fbf40c8
Robert Oikle <robert.oikle@canada.ca>
2021-02-04 16:04:37 -0500

    Added banner text to proximity viewer rollup config file

--- config/proximity-viewer.rollup.js
+++ config/proximity-viewer.rollup.js
@@ -1,16 +1,23 @@
 import babel from 'rollup-plugin-babel';
 
+const banner = 
+`/*
+ * Proximity Viewer 
+ * https://github.com/CSBP-CPSE/proximity-viewer/blob/master/LICENCE.md
+ * v1.0 - 2021-02-04
+ */`;
+
 export default {
-    input: '../web-mapping-dev/proximity-viewer/main.js',
+    input: '../proximity-viewer/src/main.js',
     output: {
-        file: '../web-mapping-prd/proximity-viewer/main.min.js',
-        format: 'iife',
-        name: 'bundle'
+        file: '../proximity-viewer/dist/main.min.js',
+		format: 'iife',
+		banner: banner
     },
     plugins: [
         babel({
             exclude: 'node_modules/**',
-			configFile: './config/babel.config.js'
+			configFile: './config/babel.config.cjs'
         })
     ]
 }
